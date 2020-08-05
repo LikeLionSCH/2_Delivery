a57611d9c5ccdd7e4b77eac82d7171a61675c8be
hyerin328115@likelion.org <hyerin328115@likelion.org>
2020-08-05 19:52:55 +0900

    models.py

--- delivery/page/models.py
+++ delivery/page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    store = models.CharField(max_length=50)
+    menu = models.CharField(max_length=50)
+    option = models.TextField()
+
+    def __str__(self):
+        return self.name
