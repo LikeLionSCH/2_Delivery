6cbe0e3968485456625a61453cd8e11fa7d20aee
bini0519 <qlstn9150@gmail.com>
2020-08-05 19:50:36 +0900

    second

--- delivery/delivery/urls.py
+++ delivery/delivery/urls.py
@@ -18,4 +18,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.order, name="order"),
+    path('record/', views.record, name="record"),
+    path('change/', views.chage, name="change"),
 ]
--- delivery/page/views.py
+++ delivery/page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def order(request):
+    return render(request, 'order.html')
+
+def record(request):
+    return render(request, 'record.html')
+
+def change(request):
+    return render(request, 'change.html')
