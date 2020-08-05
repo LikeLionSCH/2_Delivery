646c35c800eee9137cb2804853d6a230b0081e6b
hyerin328115@likelion.org <hyerin328115@likelion.org>
2020-08-05 20:16:09 +0900

    settings.py 주석

--- delivery/delivery/settings.py
+++ delivery/delivery/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'page.apps.PageConfig',
+    'page.apps.PageConfig', #앱폴더 등록
 ]
 
 MIDDLEWARE = [
