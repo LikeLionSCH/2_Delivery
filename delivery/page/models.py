5d3c4d5b5ac06f0d56c4ed88074ff964759fc360
hyerin328115@likelion.org <hyerin328115@likelion.org>
2020-08-05 20:13:28 +0900

    models.py 주석

--- delivery/page/models.py
+++ delivery/page/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    name = models.CharField(max_length=50)
-    store = models.CharField(max_length=50)
-    menu = models.CharField(max_length=50)
-    option = models.TextField()
+    name = models.CharField(max_length=50) #주문자 성명
+    store = models.CharField(max_length=50) #가게 이름
+    menu = models.CharField(max_length=50) #메뉴
+    option = models.TextField() #부수적인 옵션
 
     def __str__(self):
         return self.name
