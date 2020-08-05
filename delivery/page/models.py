ec28bf923c2dfab5c0b57a9d562f98daf14efbe9
hyerin328115@likelion.org <hyerin328115@likelion.org>
2020-08-05 20:27:13 +0900

    주소 추가

--- delivery/page/models.py
+++ delivery/page/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     name = models.CharField(max_length=50) #주문자 성명
+    address = models.TextField() #주문자 주소
     store = models.CharField(max_length=50) #가게 이름
     menu = models.CharField(max_length=50) #메뉴
     option = models.TextField() #부수적인 옵션
