8a4cea9505ce605f2512101882960a3496dd3be9
white-jang <galaxy__12@naver.com>
2020-08-05 20:21:52 +0900

    order

--- delivery/page/views.py
+++ delivery/page/views.py
@@ -11,7 +11,7 @@ def record(request):
 def change(request):
     return render(request, 'change.html')
 
-def create(request):
+def create(request): # 주문 생성
     if request.method == 'POST':
         post = Post()
         post.name = request.POST['name']
@@ -23,4 +23,23 @@ def create(request):
 
         return redirect('record')
 
+def update(request, post_id): # 주문 업데이트
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        post = Post()
+        post.name = request.POST['name']
+        post.store = request.POST['store']
+        post.address = request.POST['address']
+        post.option = request.POST['option']
+
+        post.save()
+
+        return redirect('record')
+
+
+def delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+
+    return redirect('order')
     
